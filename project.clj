86a1c1e9d3f1c53be67d49f84366bff7a4362fe7
Brad Lucas <brad@beaconhill.com>
2016-10-23 17:34:49 -0400

    update clojure version

--- project.clj
+++ project.clj
@@ -1,8 +1,5 @@
 (defproject edgardownloader "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [
-                 [org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 ]
-
-  )
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main edgardownloader.core)
